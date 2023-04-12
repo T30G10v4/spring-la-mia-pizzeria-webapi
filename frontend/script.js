01e9426ef5752d1190e636339b504c9288f9e514
T30G10v4 <matteo.giovagnotti@gmail.com>
2023-04-12 17:11:40 +0200

    Added show pizzas

--- frontend/script.js
+++ frontend/script.js
@@ -1,17 +1,30 @@
 const PIZZAS_URL = `http://localhost:8080/api/v1/pizzas`;
 
 const pizzasListDom = document.getElementById("pizzas-list");
+const formDom = document.getElementById("form");
 
 /* API */
 const getPizzaList = async () => {
 
     const response = await fetch(PIZZAS_URL);
     const jsonify = await response.json();
-    console.log(jsonify);
+    
     loadPizzaList(jsonify);
     return response;
     
 }
+
+const postPizza = async (jsonData) => {
+    const fetchOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonData,
+    };
+    const response = await fetch(PIZZAS_URL, fetchOptions);
+    return response;
+  };
 
 
 
@@ -51,4 +64,35 @@ const loadPizzaList = (list) => {
 
 }
 
+const savePizza = async (event) => {
+    // prevent default
+    event.preventDefault();
+    // read input values
+    const formData = new FormData(event.target);
+    console.log(formData)
+    const formDataObj = Object.fromEntries(formData.entries());
+    console.log(formDataObj)
+    // VALIDATION
+    // produce a json
+    const formDataJson = JSON.stringify(formDataObj);
+    console.log(formDataJson)
+    // send ajax request
+    const response = await postPizza(formDataJson);
+    console.log(response)
+    // parse response
+    const responseBody = await response.json();
+    if (response.ok) {
+      // reload data
+      loadData();
+      // reset form
+      event.target.reset();
+    } else {
+      // handle error
+      console.log('ERROR');
+      console.log(response.status);
+      console.log(responseBody);
+    }
+  };
+
+formDom.addEventListener('submit', savePizza);
 getPizzaList();
